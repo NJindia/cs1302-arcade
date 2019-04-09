7aa059bf2f8c620a6e3063c0ee05d7346f7745ae
Nikhil Jindia <nij64518@uga.edu>
2019-04-08 20:52:46 -0400

    Added Tile class

---
+++ src/main/java/cs1302/arcade/2048/Tile.java
@@ -0,0 +1,18 @@
+package cs1302.arcade.2048;
+
+public class Tile {
+    private int value;
+
+    public Tile() {
+        value = 2;
+    }
+    
+    public void setValue(int v) {
+        value = v;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
+    
