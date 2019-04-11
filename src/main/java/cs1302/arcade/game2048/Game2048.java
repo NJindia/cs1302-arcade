d333305d292b624d3ff77052bf395fef6a718219
Nikhil Jindia <nij64518@uga.edu>
2019-04-10 20:26:54 -0400

    Tried some bug-fixes

--- src/main/java/cs1302/arcade/game2048/Game2048.java
+++ src/main/java/cs1302/arcade/game2048/Game2048.java
@@ -11,18 +11,16 @@ public class Game2048 {
     private final double xMin = 0;
     private final double xMax = 640 - 100;
     private final double yMin = 0;
-    private final double yMax = 480 - 100;
-
-    
-    
+    private final double yMax = 480 - 100;    
     
     Text t = new Text("High Score: 10");
     Text t2 = new Text("Score: 10");
 
     //t.setFont(new Font(20));
-  
+    
     HBox score = new HBox(t, t2);
-
+    score.setSpacing(30);
+    
     Group group = new Group(score);
     
     Scene scene = new Scene(group, 640, 480); 
