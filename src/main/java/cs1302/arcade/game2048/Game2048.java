623dd83c31f7c47851a901c9048960c529338d7d
Nikhil Jindia <nij64518@uga.edu>
2019-04-14 18:08:52 -0400

    Revamped Tile

--- src/main/java/cs1302/arcade/game2048/Game2048.java
+++ src/main/java/cs1302/arcade/game2048/Game2048.java
@@ -5,30 +5,93 @@ import javafx.scene.*;
 import javafx.scene.control.Control;
 import javafx.scene.text.*;
 import javafx.scene.layout.*;
+import java.lang.Math;
+import java.util.*;
 
 public class Game2048 {
 
     private final double xMin = 0;
     private final double xMax = 640 - 100;
     private final double yMin = 0;
-    private final double yMax = 480 - 100;    
+    private final double yMax = 480 - 100;
     
-    Text t = new Text("High Score: 10");
-    Text t2 = new Text("Score: 10");
+    /** 4x4 array to store tiles and their positions */
+    Tile[][] tiles = new Tile[4][4];
 
-    //t.setFont(new Font(20));
-    
-    HBox score = new HBox(t, t2);
-    score.setSpacing(30);
-    
-    Group group = new Group(score);
-    
-    Scene scene = new Scene(group, 640, 480); 
+    Group tileGroup; //Stores the tiles
 
-    public Scene getScene() {
+    int score = 0;
+    //IF 2 tiles merge, add the merged tile's value to the score
+   
+    public Scene getGameScene () {
+        Text t = new Text("High Score: 10");
+        Text t2 = new Text("Score: " + score);
+        
+        t.setFont(new Font(20));
+        
+        HBox score = new HBox(t, t2);
+        score.setSpacing(30);
+    
+        Scene scene = new Scene(tileGroup, 640, 480);
         return scene;
     }
+    
+    /** move columns left or right */
+    public void moveCols (double value) {
+        
+    }
+    /** move rows up or down */
+    public void moveRows(double value) {
 
+    }
+    
+    public void moveX(int direction) {
+        //Tile[] == rows
+        for(Tile[] t1 : tiles) {
+            for (Tile t : t1) {
+                Runnable r = () -> {
+                    moveToEdgeX(t, direction);
+                };
+                Thread t = new Thread(r);
+                t.setDaemon(true);
+                t.start();
+            }
+        }
+    }
 
+    public void moveToEdgeX(Tile t, int direction) {
+        boolean collided = false;
+        switch(direction) {
+        case 1:
+            while(collided == false) {
+                t.setX(t.getX() + 1);
+                if(/*collides with another Tile t1*/) {
+                    if(t.canMerge(t1)) {
+                        //merge t into t1
+                    }
+                    collided = true;
+                } else if(t.getX() == /*last_possible_x_coordinate*/) {
+                    collided = true;
+                }
+            }
+            break;
+        case -1:
+            //Same as case 1 but for the other way
+            break;
+        default:
+            break;
+
+    }
+        
+    public void moveY(int direction) {
+        //same as moveX but with Y
+    }
+
+    public boolean addNewTile() {
+        boolean added = false;
+        Tile t = new Tile();
+        tileGroup.getChildren().add(t);
+        //Position tile?
+    }
 
 }
--- src/main/java/cs1302/arcade/game2048/Tile.java
+++ src/main/java/cs1302/arcade/game2048/Tile.java
@@ -1,26 +1,40 @@
 package cs1302.arcade.game2048;
+import javafx.scene.image.*;
 
-public class Tile {
+public class Tile extends ImageView {
     private int value;
 
+    /** Creates a new {@code Tile} and randomly sets its value to 2 or 4. */ 
     public Tile() {
-        value = 2;
+        super();
+        this.setFitWidth(100);
+        this.setPreserveRatio(true);
+        setValue(Math.pow(2, (int)((Math.random()*2) + 1)));
     }
-    
+
+    /**
+     * Sets the value of the tile and changes its image to correspond ot the new value.
+     * @param v the specified number that the tile's value will be set to
+     */
     public void setValue(int v) {
         value = v;
+        String file = "file:src/main/resources/" + v + ".png";
+        this.setImage(new Image(file));        
     }
 
     public int getValue() {
         return value;
     }
 
-    public boolean mergeTile(Tile t) {
+    public boolean canMerge(Tile t) {
         if(value == t.getValue()) {
-            value *= 2;
             return true;
+        } else {
+            return false;
         }
-        return false;
+    }
+    
+    public void mergeTile(Tile t) {
+        setValue(value * 2);
     }
 }
-    
