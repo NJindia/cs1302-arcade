2a172686734f773abe89c5340c0d3510f464a9dd
Sidharth Nair <ssn47721@uga.edu>
2019-05-06 23:14:03 -0400

    score amongsts other things

--- src/main/java/cs1302/arcade/gameTetris/GameTetris.java
+++ src/main/java/cs1302/arcade/gameTetris/GameTetris.java
@@ -26,10 +26,15 @@ public class GameTetris{
     Timeline tl = new Timeline();
     Shape currShape;
     private boolean gameOver = false;
+    private int points = 0;
+    int lines = 0;
+    int level = 1;
+
     
     public Scene getGameScene(ArcadeApp a) {
         //updateScore(0);
         app = a;
+        updateScore(0);
         score.setFont(new Font(20));
         HBox scores = new HBox(score);
         scores.setSpacing(30);
@@ -38,13 +43,93 @@ public class GameTetris{
         Button b = new Button("New Game") {
                 public void requestFocus() { } //Prevents b from taking focus
             };
-        //b.setOnAction(e -> newGame());
+        b.setOnAction(e -> newGame());
         Button b2 = new Button("Back to Games List") {
                 public void requestFocus() { } //Prevents b2 from taking focus
             };
         b2.setOnAction(e -> mainMenu());
         HBox buttons = new HBox(b, b2);
 
+/*        grid.setOnKeyPressed(createKeyHandler());
+        setTimeline(1);
+        
+        grid.setPrefSize(300, 600);
+        grid.setMaxSize(Region.USE_COMPUTED_SIZE, Region.USE_COMPUTED_SIZE);
+        
+        //adds cols and rows to grid
+        final int numCols = 10;
+        final int numRows = 20;
+        for (int i = 0; i < numCols; i++) {
+            ColumnConstraints colConst = new ColumnConstraints();
+            colConst.setPercentWidth(100.0 / numCols);
+            grid.getColumnConstraints().add(colConst);
+        }
+        for (int i = 0; i < numRows; i++) {
+            RowConstraints rowConst = new RowConstraints();
+            rowConst.setPercentHeight(100.0 / numRows);
+            grid.getRowConstraints().add(rowConst);         
+        }
+        grid.setGridLinesVisible(true);
+*/      newGrid();   
+        VBox vbox = new VBox(scores, buttons, grid);
+        scene = new Scene(vbox);
+        newShape();
+        grid.requestFocus();
+        return scene;        
+    }
+
+    private void setLinesCleared(int x)
+        {
+            System.out.println("hi12345");
+            lines=x;
+        }
+
+    private void updateScore(int lines) {
+        /*if(points == 0) {
+             this.points = 0;
+         }
+        */
+         if(lines == 1)
+         {
+             this.points += 40;
+         }
+         else if(lines == 2)
+         {
+             this.points += 100;
+         }
+         else if(lines==3)
+         {
+             this.points += 300;
+         }
+         else if(lines == 4)
+         {
+             this.points +=1200;
+         }
+         
+         String text = "Score: " + this.points;
+         score.setText(text);
+    }
+    
+    private void newGame(){
+        //    grid.getChildren().clear();
+        for(int row = 0; row<20; row++)
+        {
+            for(int col = 0; col<10; col++)
+            {
+                if(getFromGrid(col, row) != null)
+                {
+                    grid.getChildren().remove(getFromGrid(col, row));
+                }
+        
+            }
+        }
+        gameOver = false;
+        newShape();
+        newGrid();
+        //grid.setGridLinesVisible(true);
+    }
+
+    private void newGrid(){
         grid.setOnKeyPressed(createKeyHandler());
         setTimeline(1);
         
@@ -65,13 +150,9 @@ public class GameTetris{
             grid.getRowConstraints().add(rowConst);         
         }
         grid.setGridLinesVisible(true);
-        
-        VBox vbox = new VBox(scores, buttons, grid);
-        scene = new Scene(vbox);
-        newShape();
-        grid.requestFocus();
-        return scene;        
     }
+
+
 
     private void newShape() {
         String[] shapes = {"Square", "L", "J", "S", "Z", "I", "T"};
@@ -90,7 +171,7 @@ public class GameTetris{
         default:
             s = new LShape(grid);
             break;
-        }
+      }
         currShape = s;
         tl.play();
     }
@@ -147,23 +228,29 @@ public class GameTetris{
                 } //if
             } //for
             if(isFull) {
+                rowsCleared++;
                 for (int x = 0; x < 10; x++) {
                     Rectangle rect = getFromGrid(x, y);
                     grid.getChildren().remove(rect);
-
-                    System.out.println("hiiii" + x);
-                    System.out.println("y " + y);
+                    
+//                    System.out.println("hiiii" + x);
+                    //                  System.out.println("y " + y);
                     Rectangle top = getFromGrid(x, y-1);
                     if(top != null) {
+                        
+//                        System.out.println("yo " + top);
+//       grid.addRow(y, top);
                         // grid.getChildren().remove( x, y-1);
                         //grid.add(top, x, y);
-                        grid.setRowIndex(top, x);
+                        //          grid.setRowIndex(top, x);
                     }
                     
                 }
                 //clear row and add points
             }
         }
+        setLinesCleared(rowsCleared);
+        updateScore(rowsCleared);
     }
      /** Changes the scene to that of the main menu. */
     private void mainMenu() {
